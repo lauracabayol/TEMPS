c0ee1e27bffcb3be7f9738f2992baae446507b3c
Laura Cabayol Garcia <lauracabayol@lauras-macbook-pro.home>
2024-10-29 22:07:15 +0100

    fix path

--- tests/test_temps.py
+++ tests/test_temps.py
@@ -8,30 +8,34 @@ from temps.temps import TempsModule
 
 def test():
     nn_features = EncoderPhotometry()
-    nn_features.load_state_dict(torch.load("data/models/modelF_DA.pt",map_location=torch.device('cpu')))
+    nn_features.load_state_dict(
+        torch.load("data/models/modelF_DA.pt", map_location=torch.device("cpu"))
+    )
     nn_z = MeasureZ(num_gauss=6)
-    nn_z.load_state_dict(torch.load("data/models/modelF_DA.pt",map_location=torch.device('cpu')))
+    nn_z.load_state_dict(
+        torch.load("data/models/modelZ_DA.pt", map_location=torch.device("cpu"))
+    )
 
     temps_module = TempsModule(nn_features, nn_z)
 
-    col = np.array([0.54804805, 1.81142339, 0.63354394, 0.7356338 , 1.3578122 ,
-       0.90108565])
+    col = np.array(
+        [0.54804805, 1.81142339, 0.63354394, 0.7356338, 1.3578122, 0.90108565]
+    )
     ztrue = 0.4446
 
-    z, pz, odds = temps_module.get_pz(input_data=torch.Tensor(col), 
-                                            return_pz=True,
-                                            return_flag=True)
+    z, pz, odds = temps_module.get_pz(
+        input_data=torch.Tensor(col), return_pz=True, return_flag=True
+    )
 
     zdiff = (z - ztrue).abs().mean()
 
-    logger.info(f'zdiff: {zdiff}')
+    logger.info(f"zdiff: {zdiff}")
     logger.info("test passed")
 
     assert zdiff < 0.01
 
+
 test()
-
-
 
 
 # %%
